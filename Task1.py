c5b59ed176fc6ddc7b17e5e95997fe7f9fa2dd2e
mohamedhazem-commits <mohamedhazemabdelsattar@gmail.com>
2025-10-05 18:38:26 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+X = 799
+Y = 95
+Z = 2150     # 1 Define variables for the prices of three items.
+
+T_B = 3050   # 2 Define a variable for the total budget.
+
+T_C = X + Y + Z   # 3 Calculate the total cost of the items.
+
+print("Total Cost:", T_C)
+print("Budget:", T_B)
+enough_balance = True
+print("Budjet is enough:", enough_balance)     # 4 Compare the total cost with the budget.
+
+Diff = T_B - T_C 
+print("Difference:", Diff)     # 5 Calculate the difference (either how much money is left or how much more is needed).
